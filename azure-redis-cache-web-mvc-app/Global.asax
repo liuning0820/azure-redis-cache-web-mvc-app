8e7ac9aaac50d5f612cf087e5dc70ac2abff90a2
ning.liu4 <ning.liu4@hpe.com>
2017-06-14 13:26:22 +0800

    initial check in an general MVC5 project with Entity framework.

---
+++ azure-redis-cache-web-mvc-app/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="azure_redis_cache_web_mvc_app.MvcApplication" Language="C#" %>
---
+++ azure-redis-cache-web-mvc-app/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+using System.Data.Entity;
+using azure_redis_cache_web_mvc_app.Models;
+
+namespace azure_redis_cache_web_mvc_app
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Database.SetInitializer<TeamContext>(new TeamInitializer());
+        }
+    }
+}
